2bb0914c6dba28594a7bce52cafaaf32e1f72a84
Ghafoor <enzonaxari@gmail.com>
2024-10-18 18:22:10 +0000

    Added the main file and a small wordlist file.

---
+++ main.py
@@ -0,0 +1,27 @@
+import zipfile
+
+source = input("Enter the file name here: ")
+wordl = input("Enter the wordist name: (with the format like: .txt) ")
+password = ""
+
+zip_file = zipfile.ZipFile(source)
+
+with open(wordl, "rb") as file:
+    for line in file:
+        line = line.strip()
+
+        try:
+            zip_file.extractall(pwd=line)
+            #print("Password found: ", line)
+            password = line
+            break
+        except:
+            print(line)
+            continue
+
+if password == "":
+    print("Shit!, the password was not found but you can always try another wordlist")
+elif password == line:
+    print("Password found", line)
+else:
+    print("An error accured, try again. ")
