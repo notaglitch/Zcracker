03b5aff84346d600d8fcc37a35b0cad032898c5a
Ghafoor <ghafoornazari@proton.me>
2024-10-24 17:40:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import zipfile
 #Written by notaglitch, just for fun.
 #I am learning so don't judge and this script may be simple but it does the job.
-source = input("Enter the file name here: ")
+file_name = input("Enter the file name here: ")
 wordl = input("Enter the wordist name: (with the format like: .txt) ")
 password = ""
 
-zip_file = zipfile.ZipFile(source)
+zip_file = zipfile.ZipFile(file_name)
+
+if file_name[-4:] == ".zip":
+    pass
+else:
+    file_name = file_name + ".zip"
 
 with open(wordl, "rb") as file:
     for line in file:
